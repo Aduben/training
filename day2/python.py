516acc64e9ce5987eb40e13e8c7f2d93c4419cad
Asabeneh <asabeneh@gmail.com>
2021-08-06 02:31:52 +0300

    more Python scripts has been added

--- day2/python.py
+++ day2/python.py
@@ -1,4 +1,11 @@
-
+# Let's write some beautiful Python scripts
+first_name = 'Adugna'
+last_name = 'Erena'
+country = 'USA'
+skills = ['aws', 'Hadoop', 'Hive', 'MapReduce']
+print('Python is really awesome')
 print("hello my python")
 print(" what so ever")
 print(12+99+2)
+full_name = first_name + ' ' + last_name
+print(full_name, skills, country)
